619e126042aea4481f962586ffa7654baee4bbed
JoanMouba <43171207+JoanMouba@users.noreply.github.com>
2023-01-02 23:28:05 +0100

    Create 003_identificateurs.py
    JM: Add code source identificateurs

---
+++ 003_identificateurs.py
@@ -0,0 +1,15 @@
+#Intructions Python
+a = b = c = 10
+print("a=", a, "b=", b, "c=", c)
+
+d = 20
+e = 30
+
+print("d:", d, ", e:", e)
+
+nom_de_famille = "Smith"
+
+# 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+somme_de_dizaines = (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100)
+
+print(somme_de_dizaines)
