0399f04a8acfb56c3fee11cf9474cd02b6b499bd
JoanMouba <43171207+JoanMouba@users.noreply.github.com>
2023-01-02 23:10:05 +0100

    Create 002_identificateurs.py
    JM: Add code source for identificateurs

---
+++ 002_identificateurs.py
@@ -0,0 +1,20 @@
+# Definition d'identificateurs
+jourDeLaSemaine = "Lundi"  # debut de la semaine
+JourDeLaSemaineemaine = "Vendredi"
+print(jourDeLaSemaine)
+print(JourDeLaSemaineemaine)
+
+# Enlever le '#' sur chaque ligne suivante et exécuter
+# 1er_trimestre = "Janvier_Fevrier_Mars"
+# class = 'Seconde'
+# mot_@secret = 'fg#*df$'
+
+voici_la_date_de_l_obtention_de_mon_diplome = 2022
+jhwdkjhfooff = 15
+
+# Separation avec sous_tiraits
+distance_montpellier_munich_en_km = 1000
+salaireAnnuel = 44000
+
+# Camel-case
+salaireAnnuelle = 38000
