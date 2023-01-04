1681eb2084004c9c8833b3c85244387ce807e7ec
JoanMouba <43171207+JoanMouba@users.noreply.github.com>
2023-01-04 23:23:45 +0100

    JM: add code source formatage de texte_variantes

---
+++ formatage_de_texte_variantes.py
@@ -0,0 +1,7 @@
+# Variantes de formatage de texte
+test_type = "performance"
+test_number = 2672
+
+print("Il y a {} tests de {}".format(test_number, test_type))
+print(f"Il y a {test_number} tests de {test_type}")
+print("Il y a %d tests de %s" % (test_number, test_type))
