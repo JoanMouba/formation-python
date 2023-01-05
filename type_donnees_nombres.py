a8f86fb707ad62d7e9b5e7d9d2b05534edd8cef9
JoanMouba <43171207+JoanMouba@users.noreply.github.com>
2023-01-05 06:04:05 +0100

    JM: add code source type nombres

---
+++ type_donnees_nombres.py
@@ -0,0 +1,10 @@
+# Type de données natifs: Les nombres
+
+nombre_tests = 234555
+print(nombre_tests, ":", type(nombre_tests))
+
+temps_execution_tests_secs = 343255.34
+print(temps_execution_tests_secs, ":", type(temps_execution_tests_secs))
+
+nombre_complexe = 23.3 + 5j
+print(nombre_complexe, ":", type(nombre_complexe))
